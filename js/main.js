67b8903b2fddac10508b4a6ee0a132f7ff840760
Роман Соколов <sokoloff.rv@gmail.com>
2022-06-14 22:45:54 +0700

    Добавил функцию checkLength

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
  * @param {number} startNumber - Начало диапазона
  * @param {number} finishNumber - Конец диапазона
  *
- * @return {number|string} Случайное число из заданного диапазона или текст ошибки
+ * @return {number|string} Случайное число из заданного диапазона | Текст ошибки
  */
 function getRandomNumber(startNumber, finishNumber) {
   if (typeof startNumber !== 'number' || typeof finishNumber !== 'number') {
-    return 'Некорректные аргументы функции';
+    return 'Некорректный тип аргумента';
   }
   startNumber = Math.ceil(startNumber);
   finishNumber = Math.floor(finishNumber);
@@ -18,4 +18,20 @@ function getRandomNumber(startNumber, finishNumber) {
   return Math.round(Math.random() * (finishNumber - startNumber) + startNumber);
 }
 
+/**
+ * Проверяет строку на соответствие максимальной длине
+ *
+ * @param {string} text - Проверяемая строка
+ * @param {number} maxLength - Максимальная длина
+ *
+ * @return {bool|string} true, если длина строки не больше максимальной, false в противном случае | Текст ошибки
+ */
+function checkLength(text, maxLength) {
+  if (typeof text !== 'string' || typeof maxLength !== 'number') {
+    return 'Некорректный тип аргумента';
+  }
+  return text.length <= maxLength;
+}
+
 getRandomNumber(1, 10);
+checkLength('Hello world', 100);
