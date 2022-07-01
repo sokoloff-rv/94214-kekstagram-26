31bb04cba739f43cf021b019fc856f498db129f9
Роман Соколов <sokoloff.rv@gmail.com>
2022-07-01 15:38:10 +0700

    Внес исправления в  задание 7.1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-import './thumbs.js';
+import {
+  generatePhotosData
+} from './data.js';
+import {
+  generateThumbs
+} from './thumbs.js';
+
+const pictures = generatePhotosData();
+generateThumbs(pictures);
--- js/thumbs.js
+++ js/thumbs.js
@@ -1,18 +1,18 @@
-import {
-  generatePhotosData
-} from './data.js';
-
-const pictures = generatePhotosData();
 const picturesContainer = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content;
 const picturesFragment = document.createDocumentFragment();
 
-pictures.forEach((picture) => {
-  const picturesItem = picturesTemplate.cloneNode(true);
-  picturesItem.querySelector('.picture__img').src = picture.url;
-  picturesItem.querySelector('.picture__likes').textContent = picture.likes;
-  picturesItem.querySelector('.picture__comments').textContent = picture.comments.length;
-  picturesFragment.append(picturesItem);
-});
+function generateThumbs(pictures) {
+  pictures.forEach((picture) => {
+    const picturesItem = picturesTemplate.cloneNode(true);
+    picturesItem.querySelector('.picture__img').src = picture.url;
+    picturesItem.querySelector('.picture__likes').textContent = picture.likes;
+    picturesItem.querySelector('.picture__comments').textContent = picture.comments.length;
+    picturesFragment.append(picturesItem);
+  });
+  picturesContainer.append(picturesFragment);
+}
 
-picturesContainer.append(picturesFragment);
+export {
+  generateThumbs
+};
