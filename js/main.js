f1853cece5b94ef52bf8faba719da5913a658e8d
Роман Соколов <sokoloff.rv@gmail.com>
2022-07-02 04:07:07 +0700

    Выполнил задание 7.2, сделал функции стрелочными

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ import {
 import {
   generateThumbs
 } from './thumbs.js';
+import {
+  getGallery
+} from './gallery.js';
 
 const pictures = generatePhotosData();
 generateThumbs(pictures);
+
+const picturesItems = document.querySelectorAll('.picture');
+for (const pictureItem of picturesItems) {
+  const pictureId = pictureItem.querySelector('.picture__img').dataset.id;
+  const pictureData = pictures.find((picture) => picture.id === Number(pictureId));
+  pictureItem.addEventListener('click', getGallery(pictureData));
+}
--- js/thumbs.js
+++ js/thumbs.js
@@ -2,16 +2,17 @@ const picturesContainer = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content;
 const picturesFragment = document.createDocumentFragment();
 
-function generateThumbs(pictures) {
+const generateThumbs = (pictures) => {
   pictures.forEach((picture) => {
     const picturesItem = picturesTemplate.cloneNode(true);
     picturesItem.querySelector('.picture__img').src = picture.url;
+    picturesItem.querySelector('.picture__img').dataset.id = picture.id;
     picturesItem.querySelector('.picture__likes').textContent = picture.likes;
     picturesItem.querySelector('.picture__comments').textContent = picture.comments.length;
     picturesFragment.append(picturesItem);
   });
   picturesContainer.append(picturesFragment);
-}
+};
 
 export {
   generateThumbs
