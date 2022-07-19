b9ace5b7f543bc7c3d5055715d5bd1d0cf48044a
Роман Соколов <sokoloff.rv@gmail.com>
2022-07-19 18:51:05 +0700

    Причесал константы для улучшения читаемости

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ import {
   getRandomNumber
 } from './functions.js';
 
-const photosDescriptions = [
+const PHOTOS_DESCRIPTIONS = [
   'Пляж',
   'Указатель на пляж',
   'Море',
@@ -29,8 +29,7 @@ const photosDescriptions = [
   'Концерт',
   'Шок-контент, бегемот хочет съесть туристов вместе с машиной'
 ];
-
-const commentsMessages = [
+const COMMENTS_MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -38,8 +37,7 @@ const commentsMessages = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-
-const commentsAuthorsNames = [
+const COMMENTS_AUTHORS_NAMES = [
   'Петя',
   'Вася',
   'Света',
@@ -61,10 +59,17 @@ const commentsAuthorsNames = [
   'Вавилонянин',
   'Малонемножков Константин Вениаминович'
 ];
+const NUMBER_OF_POSTS = 25;
+const NUMBER_OF_AVATAR_PICTURES = 6;
+const MAX_MESSAGES_PER_COMMENT = 2;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 3;
+const MAX_COMMENTS = 30;
 
 const getCommentMessage = (quantity) => {
   let commentMessage = '';
-  const tempCommentsMessages = commentsMessages.slice(0);
+  const tempCommentsMessages = COMMENTS_MESSAGES.slice(0);
   for (let i = 0; i < quantity; i++) {
     commentMessage += tempCommentsMessages[getRandomNumber(0, tempCommentsMessages.length - 1)];
     const commentMessageIndex = tempCommentsMessages.indexOf(commentMessage);
@@ -82,9 +87,9 @@ const getComments = (quantity) => {
   for (let i = 0; i < quantity; i++) {
     comments[i] = {
       id: createIdForComments(),
-      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-      message: getCommentMessage(getRandomNumber(1, 2)),
-      name: commentsAuthorsNames[getRandomNumber(0, commentsAuthorsNames.length - 1)]
+      avatar: `img/avatar-${getRandomNumber(1, NUMBER_OF_AVATAR_PICTURES)}.svg`,
+      message: getCommentMessage(getRandomNumber(1, MAX_MESSAGES_PER_COMMENT)),
+      name: COMMENTS_AUTHORS_NAMES[getRandomNumber(0, COMMENTS_AUTHORS_NAMES.length - 1)]
     };
   }
   return comments;
@@ -92,13 +97,13 @@ const getComments = (quantity) => {
 
 const generatePhotosData = () => {
   const photosData = [];
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < NUMBER_OF_POSTS; i++) {
     photosData[i] = {
       id: i + 1,
       url: `photos/${i + 1}.jpg`,
-      description: photosDescriptions[i],
-      likes: getRandomNumber(15, 200),
-      comments: getComments(getRandomNumber(4, 12))
+      description: PHOTOS_DESCRIPTIONS[i],
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+      comments: getComments(getRandomNumber(MIN_COMMENTS, MAX_COMMENTS))
     };
   }
   return photosData;
