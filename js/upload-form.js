ee971401586deacae3220337dcbe94b43c4a007c
Роман Соколов <sokoloff.rv@gmail.com>
2022-07-20 16:54:53 +0700

    Внес небольшие исправления в форму загузки

--- js/upload-form.js
+++ js/upload-form.js
@@ -8,26 +8,23 @@ const uploadFormComment = uploadForm.querySelector('.text__description');
 const openUploadForm = () => {
   uploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
-
   document.addEventListener('keydown', onPopupEscKeydown);
-
-  const closeUploadForm = () => {
-    uploadOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onPopupEscKeydown);
-    uploadForm.reset();
-  };
-
-  uploadFormClose.addEventListener('click', () => {
-    closeUploadForm();
-  });
-
-  function onPopupEscKeydown(event) {
-    if (event.key === 'Escape' && ![uploadFormComment, uploadFormHashtag].includes(document.activeElement)) {
-      closeUploadForm();
-    }
-  }
 };
+
+const closeUploadForm = () => {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  uploadForm.reset();
+};
+
+uploadFormClose.addEventListener('click', closeUploadForm);
+
+function onPopupEscKeydown(event) {
+  if (event.key === 'Escape' && ![uploadFormComment, uploadFormHashtag].includes(document.activeElement)) {
+    closeUploadForm();
+  }
+}
 
 export {
   openUploadForm
