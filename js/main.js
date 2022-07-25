473c2a076b3e28780358929cf7c77e188e9ad909
Роман Соколов <sokoloff.rv@gmail.com>
2022-07-25 17:43:33 +0700

    Добавил отображение превью изображения

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ import {
   renderBigPicture
 } from './big-picture.js';
 import './upload-form-toggle.js';
+import './preview-photo.js';
 
 const filterElement = document.querySelector('.img-filters');
 const picturesElement = document.querySelector('.pictures');
